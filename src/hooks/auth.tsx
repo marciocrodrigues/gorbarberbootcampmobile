3a20afa5a10f5a249b104752495fd683cf76d711
Marcio Costa Rodrigues <marciodeath@hotmail.com>
2021-02-16 15:04:32 -0300

    desenvolvimento telas

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -2,9 +2,16 @@ import React, { createContext, useCallback, useState, useContext, useEffect } fr
 import AsyncStorage from '@react-native-community/async-storage';
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  avatar_url: string;
+}
+
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
 interface SignInCredentials {
@@ -13,7 +20,7 @@ interface SignInCredentials {
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   loading: boolean;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
@@ -35,6 +42,8 @@ const AuthProvider: React.FC = ({ children }) => {
         ]);
 
         if (token[1] && user[1]) {
+          api.defaults.headers.authorization = `Bearer ${token[1]}`;
+
           setData({ token: token[1], user: JSON.parse(user[1]) });
         }
 
@@ -47,12 +56,16 @@ const AuthProvider: React.FC = ({ children }) => {
   const signIn = useCallback(async ({ email, password }: SignInCredentials) => {
     const response = await api.post('sessions', { email, password });
 
+    console.log(response);
+
     const { token, user } = response.data;
 
     await AsyncStorage.multiSet([
       ['@GoBarber:token', token],
       ['@GoBarber:user', JSON.stringify(user)],
     ]);
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
